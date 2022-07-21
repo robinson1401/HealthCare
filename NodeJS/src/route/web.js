80c645ed8d74aa44d8855d67deb8cdd0e99b29c9
robinson1401 <huuson1401@gmail.com>
2022-07-21 14:42:33 +0700

    update render to view

--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -1,5 +1,5 @@
 import express from "express";
-import homeController from "../controllers/homeController";
+import homeController from "../controllers/homeController"
 
 let router = express.Router();
 
