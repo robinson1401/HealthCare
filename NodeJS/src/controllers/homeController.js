94229e79e7d3d943016b2d17cac35d18b8add52d
robinson1401 <huuson1401@gmail.com>
2022-07-22 22:53:24 +0700

    ORM update

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -24,12 +24,33 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUsers();
-    console.log('-----------------------------')
-    console.log(data);
-    console.log('-----------------------------')
+    // console.log('-----------------------------')
+    // console.log(data);
+    // console.log('-----------------------------')
 
     return res.render('displayCRUD.ejs', {
         dataTable: data,
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        })
+    } else {
+        return res.send('User not found');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers,
     })
 }
 
@@ -38,4 +59,6 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
