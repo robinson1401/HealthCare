980b3594e90822a8b74f7653e11a07395df13584
robinson1401 <huuson1401@gmail.com>
2022-07-22 23:27:49 +0700

    edit delete

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -54,6 +54,16 @@ let putCRUD = async (req, res) => {
     })
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete the user succeed');
+    } else {
+        return res.send('User not found');
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
@@ -61,4 +71,5 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- NodeJS/src/services/CRUDService.js
+++ NodeJS/src/services/CRUDService.js
@@ -98,9 +98,26 @@ let updateUserData = (data) => {
 
 }
 
+let deleteUserById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId }
+            })
+            if (user) {
+                await user.destroy();
+            }
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     createNewUser: createNewUser,
     getAllUsers: getAllUsers,
     getUserInfoById: getUserInfoById,
     updateUserData: updateUserData,
+    deleteUserById: deleteUserById,
 }
