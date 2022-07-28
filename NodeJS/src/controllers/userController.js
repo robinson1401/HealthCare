69b12cd6163774b4b9c6c4449ad31b90f86d8563
robinson1401 <huuson1401@gmail.com>
2022-07-28 15:55:41 +0700

    edit, delete user ReactJs

--- NodeJS/src/controllers/userController.js
+++ NodeJS/src/controllers/userController.js
@@ -1,4 +1,4 @@
-import userService from "../services/userServiec";
+import userService from "../services/userService";
 
 let handleLogin = async (req, res) => {
     let email = req.body.email;
@@ -38,7 +38,32 @@ let handleGetAllUsers = async (req, res) => {
     })
 }
 
+let handleCreatNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body);
+    return res.status(200).json(message);
+}
+
+let handleEditUser = async (req, res) => {
+    let data = req.body;
+    let message = await userService.editUser(data);
+    return res.status(200).json(message);
+}
+
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message);
+}
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUsers: handleGetAllUsers,
+    handleCreatNewUser: handleCreatNewUser,
+    handleEditUser: handleEditUser,
+    handleDeleteUser: handleDeleteUser,
 }
--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -16,6 +16,9 @@ let initWebRoutes = (app) => {
 
     router.post('/api/login', userController.handleLogin);
     router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreatNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser); // restAPI
 
     return app.use("/", router);
 }
