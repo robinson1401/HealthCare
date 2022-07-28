9c3fbd7f32f6cd668c97da5357001162e043f572
robinson1401 <huuson1401@gmail.com>
2022-07-28 07:06:17 +0700

    read data

--- ReactJS/src/containers/System/UserManage.js
+++ ReactJS/src/containers/System/UserManage.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import "./UserManage.scss";
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrayUsers: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrayUsers: response.users
+            }, () => {
+            });
+        }
     }
 
 
     render() {
+        console.log('check state', this.state);
+        let arrayUsers = this.state.arrayUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className='title text-center'>
+                    Manage users
+                </div>
+                <div className="users-table mt-3 mx-1">
+                    <table class="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Email</th>
+                                <th scope="col">First name</th>
+                                <th scope="col">Last name</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                arrayUsers && arrayUsers.map((item, index) => {
+                                    console.log('check map item', item, index);
+                                    return (
+                                        <tr>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                                <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
