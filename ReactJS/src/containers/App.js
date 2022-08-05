bb238d7175bedc76309335437100eedddd67ff80
robinson1401 <huuson1401@gmail.com>
2022-08-05 15:36:25 +0700

    update

--- ReactJS/src/containers/App.js
+++ ReactJS/src/containers/App.js
@@ -16,9 +16,9 @@ import Login from './Auth/Login';
 
 import Header from './Header/Header';
 import System from '../routes/System';
+import HomePage from './HomePage/HomePage.js';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
-import ConfirmModal from '../components/ConfirmModal';
 
 class App extends Component {
 
@@ -45,7 +45,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
@@ -53,6 +52,8 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={HomePage} />
+
                             </Switch>
                         </span>
 
